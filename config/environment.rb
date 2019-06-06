b4dac8dcff74e4fce11ac6fc0965e87fd449b3b2
Alethea Cary <aletheacary@gmail.com>
2019-06-06 03:42:40 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,14 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+ require_relative '../lib/concerns/findable'
+ require_relative '../lib/concerns/memorable'
+ require_relative '../lib/concerns/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+song = Song.new
+artist = Artist.new
+
+puts Artist.metadata, Artist.find, artist.to_param
+puts Song.metadata, Song.find, song.to_param
